69d2450686646cd9ad62f96d2d756f0b5d0019e9
Maxim <ScorpionEvil777@gmail.com>
2022-07-23 00:37:33 +0300

    Последнее задание в изучении инициализатора

--- Lesson_1/Lesson_1.5/lesson_1.5_part_8.py
+++ Lesson_1/Lesson_1.5/lesson_1.5_part_8.py
@@ -40,3 +40,56 @@ price - цена.
 
 P.S. Отображать на экране ничего не нужно, только создать объекты по указанным требованиям.
 """
+class Cart:
+    def __init__(self):
+        self.goods = []
+
+    # добавление в корзину товара, представленного объектом gd
+    def add(self, *gd):
+        for val in gd:
+            self.goods.append(val)
+        return self.goods
+
+    # удаление из корзины товара по индексу indx
+    def remove(self, indx):
+        del self.goods[indx]
+        return self.goods
+
+
+    # получение из корзины товаров в виде списка из строк
+    def get_list(self):
+        return [f"{i.name}: {i.price}" for i in self.goods]
+
+
+class Table:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class TV:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class Notebook:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class Cup:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+cart = Cart() # создаю корзину
+gd1 = TV('Sony', 50000)
+gd2 = TV('Samsung', 65999)
+gd3 = Table('Пенёк лесной', 150000)
+gd4 = Notebook('Asus', 76000)
+gd5 = Notebook('Lenovo', 56000)
+gd6 = Cup('Graal', 25000)
+cart.add(gd1, gd2, gd3, gd4, gd5, gd6)# заполняю корзину товаром
+print(cart.get_list())
